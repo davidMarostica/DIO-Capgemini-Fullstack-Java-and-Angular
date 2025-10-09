c671e12846872c01d85a85df61ead7bee2e504cf
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-10-09 12:57:17 -0300

    Atualiza pom.xml e SpringbootApplication.java; adiciona context.xml

--- introdução ao framework Spring Boot/springboot/src/main/java/com/digitalinnovetionone/springboot/SpringbootApplication.java
+++ introdução ao framework Spring Boot/springboot/src/main/java/com/digitalinnovetionone/springboot/SpringbootApplication.java
@@ -2,12 +2,19 @@ package com.digitalinnovetionone.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- IMPORTANTE!
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer { // <--- 1. HERDAR ISTO!
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
 
+    // <--- 2. ADICIONAR ESTE MÉTODO!
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootApplication.class);
+    }
 }
